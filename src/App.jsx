daea2e6208092e3349e1cf8f6b567e3b4e12cf00
plebeius.eth <mottoes_recap0u@icloud.com>
2023-09-28 16:16:35 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className='App'>
+      test
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useDefaultSubplebbits.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useDefaultSubplebbits = () => {
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const value = await fetch('https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/subplebbits.json', { cache: 'no-cache' }).then((res) =>
+          res.json(),
+        );
+        setValue(value);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+  return value;
+};
+
+export default useDefaultSubplebbits;
