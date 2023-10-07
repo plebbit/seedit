5857fdff33af1c83ea575c7962accfbc5e8cf38d
plebeius.eth <mottoes_recap0u@icloud.com>
2023-10-07 14:50:35 +0200

    chore: add translations

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './lib/init-translations';
 import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
