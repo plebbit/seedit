2ccdd0339e84f4807add11ebe3ca973be963b271
plebeius.eth <mottoes_recap0u@icloud.com>
2023-10-09 12:21:21 +0200

    chore: add css themes, zustand, use kebab case

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-
+import { useEffect } from 'react';
+import useTheme from './hooks/use-theme';
 
 function App() {
+  const [theme] = useTheme();
+
+  useEffect(() => {
+    document.body.classList.forEach((className) => document.body.classList.remove(className))
+    document.body.classList.add(theme)
+  }, [theme])
+
   return (
     <div className='App'>
       test
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './lib/init-translations';
+import App from './app';
 import { HashRouter as Router } from 'react-router-dom';
+import './lib/init-translations';
+import './index.css';
+import './themes.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
