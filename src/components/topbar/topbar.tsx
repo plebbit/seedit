46e12a578f267e386ae1f068bc33a9e5cc92d556
plebeius.eth <mottoes_recap0u@icloud.com>
2023-10-10 21:00:40 +0200

    fix(topbar): classname typo

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,7 +1,7 @@
 import { FC, useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import style from './topbar.module.css';
+import styles from './topbar.module.css';
 import { useAccount } from '@plebbit/plebbit-react-hooks';
 import useDefaultSubplebbits from '../../hooks/use-default-subplebbits';
 
@@ -33,49 +33,49 @@ const TopBar: FC = () => {
   }, []);
 
   return (
-    <div className={style.headerArea}>
-      <div className={style.widthClip}>
-        <div className={style.dropdown} ref={dropdownRef}>
-          <span className={style.selectedTitle} onClick={toggleClick}>
+    <div className={styles.headerArea}>
+      <div className={styles.widthClip}>
+        <div className={styles.dropdown} ref={dropdownRef}>
+          <span className={styles.selectedTitle} onClick={toggleClick}>
             {t('topbar_my_subs')}
           </span>
         </div>
-        <div className={style.dropChoices} style={{ display: isClicked ? 'block' : 'none' }}>
+        <div className={styles.dropChoices} style={{ display: isClicked ? 'block' : 'none' }}>
           {subscriptions?.map((subscription: string, index: number) => (
-            <Link key={index} to={`p/${subscription}`} className={style.choice} onClick={(event) => event.preventDefault()}>
+            <Link key={index} to={`p/${subscription}`} className={styles.choice} onClick={(event) => event.preventDefault()}>
               {subscription}
             </Link>
           ))}
         </div>
-        <div className={style.srList}>
-          <ul className={style.srBar}>
+        <div className={styles.srList}>
+          <ul className={styles.srBar}>
             <li>
-              <Link to='/' className={style.selected}>
+              <Link to='/' className={styles.selected}>
                 {t('topbar_home')}
               </Link>
             </li>
             <li>
-              <span className={style.separator}>-</span>
-              <Link to='/p/all' className={style.choice} onClick={(event) => event.preventDefault()}>
+              <span className={styles.separator}>-</span>
+              <Link to='/p/all' className={styles.choice} onClick={(event) => event.preventDefault()}>
                 {t('topbar_all')}
               </Link>
             </li>
           </ul>
-          <span style={{ cursor: 'default' }} className={style.separator}>
+          <span style={{ cursor: 'default' }} className={styles.separator}>
               |  
           </span>
-          <ul className={style.srBar}>
+          <ul className={styles.srBar}>
             {ethFilteredAddresses?.map((address: string, index: number) => (
               <li key={index}>
-                {index !== 0 && <span className={style.separator}>-</span>}
-                <Link to={`p/${address}`} className={style.choice} onClick={(event) => event.preventDefault()}>
+                {index !== 0 && <span className={styles.separator}>-</span>}
+                <Link to={`p/${address}`} className={styles.choice} onClick={(event) => event.preventDefault()}>
                   {address.slice(0, -4)}
                 </Link>
               </li>
             ))}
           </ul>
         </div>
-        <Link to='#' className={style.moreLink}>
+        <Link to='#' className={styles.moreLink}>
           {t('topbar_edit')} »
         </Link>
       </div>
