832001bed29058692574298748b8f6c24c0b0407
plebeius.eth <mottoes_recap0u@icloud.com>
2023-10-25 21:31:25 +0200

    feat(comments): add textarea section

--- src/components/views/comments/comments.tsx
+++ src/components/views/comments/comments.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useComment } from '@plebbit/plebbit-react-hooks';
 import styles from './comments.module.css';
@@ -9,6 +9,9 @@ import Post from '../../post';
 const Comments: FC = () => {
   const { commentCid } = useParams();
   const comment = useComment({ commentCid });
+  const { replyCount } = comment || {};
+
+  useEffect(() => {window.scrollTo(0, 0);}, []);
 
   return (
     <>
@@ -17,6 +20,30 @@ const Comments: FC = () => {
         <Header />
         <div className={styles.comments}>
           <Post post={comment} shouldExpand={false} />
+          <div className={styles.commentArea}>
+            <div className={styles.commentsTitle}>
+              <span className={styles.title}>all {replyCount} comments</span>
+            </div>
+            <div className={styles.menuArea}>
+              <div className={styles.spacer}>
+                <span className={styles.dropdownTitle}>sorted by:</span>
+                <div className={styles.dropdown}>
+                  <span className={styles.selected}>best</span>
+                </div>
+              </div>
+              <div className={styles.mdContainer}>
+                <div className={styles.md}>
+                  <textarea className={styles.textarea} />
+                </div>
+                <div className={styles.bottomArea}>
+                  <button className={styles.save}>save</button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.replies}>
+
+            </div>
+          </div>
         </div>
       </div>
     </>
