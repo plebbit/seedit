1c11ff84184797eb274e2c2d1cd37518939c25ea
plebeius.eth <mottoes_recap0u@icloud.com>
2023-11-04 18:58:58 +0100

    chore(translations): title, text, required, optional, community address

--- src/views/submit/submit.tsx
+++ src/views/submit/submit.tsx
@@ -27,19 +27,21 @@ const Submit = () => {
         <div className={styles.formContent}>
           <div className={styles.field}>
             <span className={styles.fieldTitleOptional}>url</span>
+            <span className={styles.optional}> ({t('optional')})</span>
             <div className={styles.fieldContent}>
               <input className={`${styles.input} ${styles.inputUrl}`} type='text' />
               <div className={styles.description}>{t('submit_url_description')}</div>
             </div>
           </div>
           <div className={styles.field}>
-            <span className={styles.fieldTitleRequired}>title</span>
+            <span className={styles.fieldTitleRequired}>{t('title')}</span>
             <div className={styles.fieldContent}>
               <textarea className={`${styles.input} ${styles.inputTitle}`} />
             </div>
           </div>
           <div className={styles.field}>
-            <span className={styles.fieldTitleOptional}>text</span>
+            <span className={styles.fieldTitleOptional}>{t('text')}</span>
+            <span className={styles.optional}> ({t('optional')})</span>
             <div className={styles.fieldContent}>
               <textarea className={`${styles.input} ${styles.inputText}`} />
             </div>
@@ -47,14 +49,14 @@ const Submit = () => {
           <div className={styles.field}>
             <span className={styles.fieldTitleRequired}>choose where to post</span>
             <div className={styles.fieldContent}>
-              <span className={styles.fieldSubtitle}>Community address:</span>
+              <span className={styles.fieldSubtitle}>{t('community address')}:</span>
               <input className={`${styles.input} ${styles.inputCommunity}`} type='text' defaultValue={subplebbitAddress} />
             </div>
           </div>
           <div className={`${styles.field} ${styles.notice}`}>please be mindful of the community's rules, seedit does not have global admins.</div>
-          <div>*required</div>
+          <div>*{t('required')}</div>
           <div className={styles.submit}>
-            <button className={styles.submitButton}>submit</button>
+            <button className={styles.submitButton}>{t('submit')}</button>
           </div>
         </div>
       </div>
