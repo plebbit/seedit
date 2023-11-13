cc9a040d7c5ec3a0633a729941ab7bc352a7df04
plebeius.eth <mottoes_recap0u@icloud.com>
2023-11-13 11:48:48 +0100

    feat(account bar): add search bar to connect to a sub

--- src/components/header/account-bar/account-bar.tsx
+++ src/components/header/account-bar/account-bar.tsx
@@ -1,13 +1,17 @@
-import { useTranslation } from 'react-i18next';
+import { useEffect, useRef, useState } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
-import styles from './account-bar.module.css';
 import { useAccount } from '@plebbit/plebbit-react-hooks';
+import { useTranslation } from 'react-i18next';
+import styles from './account-bar.module.css';
 
 const AccountBar = () => {
   const account = useAccount();
   const { t } = useTranslation();
   const location = useLocation();
   const { subplebbitAddress } = useParams();
+  const [searchHidden, setSearchHidden] = useState(true);
+  const searchBarRef = useRef<HTMLDivElement>(null);
+  const searchInputRef = useRef<HTMLInputElement>(null);
   let submitLink;
 
   if (location.pathname.startsWith(`/p/${subplebbitAddress}/`)) {
@@ -16,30 +20,55 @@ const AccountBar = () => {
     submitLink = '/submit';
   }
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (searchBarRef.current && event.target instanceof Node && !searchBarRef.current.contains(event.target)) {
+      setSearchHidden(true);
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!searchHidden && searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }, [searchHidden]);
+
   return (
-    <div className={styles.header}>
-      <span className={styles.user}>
-        <Link to='/user' onClick={(e) => e.preventDefault()}>
-          {account?.author?.shortAddress}
+    <>
+      <div className={styles.header}>
+        <span className={styles.user}>
+          <Link to='/user' onClick={(e) => e.preventDefault()}>
+            {account?.author?.shortAddress}
+          </Link>
+        </span>
+        <span className={styles.separator}>|</span>
+        <Link to={submitLink} className={styles.textButton}>
+          {t('submit')}
         </Link>
-      </span>
-      <span className={styles.separator}>|</span>
-      <Link to={submitLink} className={styles.textButton}>
-        {t('submit')}
-      </Link>
-      <span className={styles.separator}>|</span>
-      <Link to='/settings' className={styles.iconButton} onClick={(e) => e.preventDefault()}>
-        ✉️
-      </Link>
-      <span className={styles.separator}>|</span>
-      <Link to='/settings' className={styles.iconButton} onClick={(e) => e.preventDefault()}>
-        🔎
-      </Link>
-      <span className={styles.separator}>|</span>
-      <Link to='/settings' className={styles.textButton}>
-        {t('preferences')}
-      </Link>
-    </div>
+        <span className={styles.separator}>|</span>
+        <Link to='/settings' className={styles.iconButton} onClick={(e) => e.preventDefault()}>
+          ✉️
+        </Link>
+        <span className={styles.separator}>|</span>
+        <span className={styles.iconButton} onClick={() => setSearchHidden(false)}>
+          🔎
+        </span>
+        <span className={styles.separator}>|</span>
+        <Link to='/settings' className={styles.textButton}>
+          {t('preferences')}
+        </Link>
+      </div>
+      <div className={styles.searchBar} style={{visibility: searchHidden ? 'hidden' : 'visible'}} ref={searchBarRef}>
+        <input type='text' placeholder={`"community.eth" ${t('or')} "12D3KooW..."`} ref={searchInputRef} />
+        <input type='submit' value='' />
+      </div>
+    </>
   );
 };
 
