7cc53164b9655e96b9cebb7f7a7823b473409641
plebeius.eth <mottoes_recap0u@icloud.com>
2023-11-22 10:37:57 +0100

    chore(translations): join, leave

--- src/components/subscribe-button/subscribe-button.tsx
+++ src/components/subscribe-button/subscribe-button.tsx
@@ -1,5 +1,6 @@
 import { useSubscribe } from '@plebbit/plebbit-react-hooks';
 import styles from './subscribe-button.module.css';
+import { useTranslation } from 'react-i18next';
 
 interface subscribeButtonProps {
   address: string | undefined;
@@ -7,6 +8,7 @@ interface subscribeButtonProps {
 
 const SubscribeButton = ({ address }: subscribeButtonProps) => {
   const { subscribe, subscribed, unsubscribe } = useSubscribe({ subplebbitAddress: address });
+  const { t } = useTranslation();
 
   const handleSubscribe = async () => {
     try {
@@ -27,7 +29,7 @@ const SubscribeButton = ({ address }: subscribeButtonProps) => {
 
   return (
     <span className={`${styles.subscribeButton} ${subscribed ? styles.leaveButton : styles.joinButton}`} onClick={handleSubscribe}>
-      {subscribed ? 'leave' : 'join'}
+      {subscribed ? `${t('leave')}` : `${t('join')}`}
     </span>
   );
 };
