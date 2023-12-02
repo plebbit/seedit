2f578217cd975e66475dea52d2b99f7b3c050421
plebeius.eth <mottoes_recap0u@icloud.com>
2023-12-02 17:00:16 +0100

    refactor(post tools): use components instead of variables

--- src/components/post/post-tools/post-tools.tsx
+++ src/components/post/post-tools/post-tools.tsx
@@ -1,3 +1,4 @@
+import { useAccount, useSubplebbit } from '@plebbit/plebbit-react-hooks';
 import styles from './post-tools.module.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -6,20 +7,28 @@ import { FailedLabel, PendingLabel, SpoilerLabel } from '../label';
 interface PostToolsProps {
   cid: string;
   failed?: boolean;
+  hasLabel?: boolean;
   isReply?: boolean;
-  replyCount: number;
+  replyCount?: number;
   spoiler?: boolean;
-  subplebbitAddress: string;
+  subplebbitAddress?: string;
   showReplyForm?: () => void;
 }
 
-const PostTools = ({ cid, failed, isReply, replyCount, spoiler, subplebbitAddress, showReplyForm }: PostToolsProps) => {
+const ModTools = () => {
+  return (
+    <li className={styles.button}>
+      <span>moderate</span>
+    </li>
+  );
+};
+
+const ThreadTools = ({ cid, hasLabel, subplebbitAddress, replyCount = 0 }: PostToolsProps) => {
   const { t } = useTranslation();
   const validReplyCount = isNaN(replyCount) ? 0 : replyCount;
   const commentCount = validReplyCount === 0 ? t('post_no_comments') : `${validReplyCount} ${validReplyCount === 1 ? t('post_comment') : t('post_comments')}`;
-  const hasLabel = spoiler || (cid === undefined && !isReply);
 
-  const postTools = (
+  return (
     <>
       <li className={`${styles.button} ${!hasLabel ? styles.firstButton : ''}`}>
         <Link to={`/p/${subplebbitAddress}/c/${cid}`}>{commentCount}</Link>
@@ -41,8 +50,11 @@ const PostTools = ({ cid, failed, isReply, replyCount, spoiler, subplebbitAddres
       </li>
     </>
   );
+};
 
-  const replyTools = (
+const ReplyTools = ({ cid, hasLabel, showReplyForm }: PostToolsProps) => {
+  const { t } = useTranslation();
+  return (
     <>
       <li className={`${styles.button} ${!hasLabel ? styles.firstButton : ''}`}>
         <span>{t('reply_permalink')}</span>
@@ -61,19 +73,33 @@ const PostTools = ({ cid, failed, isReply, replyCount, spoiler, subplebbitAddres
       </li>
     </>
   );
+};
 
-  const label = (
+const PostToolsLabel = ({ cid, failed, isReply, spoiler }: PostToolsProps) => {
+  return (
     <span className={styles.label}>
       {spoiler && <SpoilerLabel />}
       {cid === undefined && !isReply && !failed && <PendingLabel />}
       {failed && <FailedLabel />}
     </span>
   );
+};
+
+const PostTools = ({ cid, failed, hasLabel = false, isReply, replyCount, spoiler, subplebbitAddress, showReplyForm }: PostToolsProps) => {
+  const account = useAccount();
+  const role = useSubplebbit({ subplebbitAddress })?.roles?.[account?.author?.address]?.role;
+  const isMod = role === 'admin' || role === 'owner' || role === 'moderator';
+  hasLabel = spoiler || (cid === undefined && !isReply);
 
   return (
     <ul className={`${styles.buttons} ${isReply ? styles.buttonsReply : ''} ${hasLabel ? styles.buttonsLabel : ''}`}>
-      {hasLabel && label}
-      {isReply ? replyTools : postTools}
+      {hasLabel && <PostToolsLabel cid={cid} failed={failed} isReply={isReply} spoiler={spoiler} />}
+      {isReply ? (
+        <ReplyTools cid={cid} hasLabel={hasLabel} showReplyForm={showReplyForm} />
+      ) : (
+        <ThreadTools cid={cid} hasLabel={hasLabel} subplebbitAddress={subplebbitAddress} replyCount={replyCount} />
+      )}
+      {isMod && <ModTools />}
     </ul>
   );
 };
