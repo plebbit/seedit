1b072b1cb81d27d8c0c101ef5a6182c81da69921
plebeius.eth <mottoes_recap0u@icloud.com>
2023-12-06 16:10:08 +0100

    refactor(settings): extract each setting as sole component, import in settings view component

--- src/views/settings/settings.tsx
+++ src/views/settings/settings.tsx
@@ -8,203 +8,12 @@ import {
   setActiveAccount,
   useAccount,
   useAccounts,
-  useResolvedAuthorAddress,
 } from '@plebbit/plebbit-react-hooks';
-import { getShortAddress } from '@plebbit/plebbit-js';
 import stringify from 'json-stringify-pretty-compact';
-import useTheme from '../../hooks/use-theme';
 import styles from './settings.module.css';
-
-// prettier-ignore
-const availableLanguages = ['ar', 'bn', 'cs', 'da', 'de', 'el', 'en', 'es', 'fa', 'fi', 'fil', 'fr', 'he', 'hi', 'hu', 'id', 'it', 'ja', 'ko', 'mr', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sq', 'sv', 'te', 'th', 'tr', 'uk', 'ur', 'vi', 'zh'];
-
-const LanguageSettings = () => {
-  const { i18n } = useTranslation();
-  const { changeLanguage, language } = i18n;
-
-  const onSelectLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    changeLanguage(e.target.value);
-  };
-
-  return (
-    <span className={styles.categorySettings}>
-      <select value={language} onChange={onSelectLanguage}>
-        {availableLanguages.map((lang) => (
-          <option key={lang} value={lang}>
-            {lang}
-          </option>
-        ))}
-      </select>
-    </span>
-  );
-};
-
-const ThemeSettings = () => {
-  const [theme, setTheme] = useTheme();
-  const { t } = useTranslation();
-
-  return (
-    <span className={styles.categorySettings}>
-      <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-        <option value='light'>{t('light')}</option>
-        <option value='dark'>{t('dark')}</option>
-      </select>
-    </span>
-  );
-};
-
-const ProfileSettings = () => {
-  const account = useAccount();
-  const [username, setUsername] = useState(account?.author.displayName || '');
-  const [savedUsername, setSavedUsername] = useState(false);
-
-  useEffect(() => {
-    if (savedUsername) {
-      setTimeout(() => {
-        setSavedUsername(false);
-      }, 2000);
-    }
-  }, [savedUsername]);
-
-  const saveUsername = async () => {
-    try {
-      await setAccount({ ...account, author: { ...account?.author, displayName: username } });
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message);
-        console.log(error);
-      } else {
-        console.error('An unknown error occurred:', error);
-      }
-    }
-    setSavedUsername(true);
-  };
-
-  const [cryptoAddress, setCryptoAddress] = useState('');
-  const [cryptoAddressToResolve, setCryptoAddressToResolve] = useState('');
-  const [checkingCryptoAddress, setCheckingCryptoAddress] = useState(false);
-  const [showResolvingMessage, setShowResolvingMessage] = useState(true);
-  const [savedCryptoAddress, setSavedCryptoAddress] = useState(false);
-  const [resolveString, setResolveString] = useState('');
-  const [resolveClass, setResolveClass] = useState('');
-
-  const author = { ...account?.author, address: cryptoAddressToResolve };
-  const {resolvedAddress, state, error, chainProvider } = useResolvedAuthorAddress({ author, cache: false });
-
-  useEffect(() => {
-    if (showResolvingMessage) {
-      if (state === 'failed') {
-        if (error instanceof Error) {
-          setResolveString('failed to resolve crypto address, error: ' + error.message)
-        } else {
-          setResolveString('cannot resolve crypto address, unknown error')
-        }
-        setResolveClass(styles.red)
-      } else if (state === 'resolving') {
-        setResolveString(`resolving from ${chainProvider?.urls}`)
-        setResolveClass(styles.yellow)
-      }
-    }
-  }, [showResolvingMessage, state, error, chainProvider]);
-
-  useEffect(() => {
-    if (resolvedAddress && resolvedAddress === account?.signer.address) {
-      setResolveString('crypto address belongs to this account, address: ' + getShortAddress(resolvedAddress));
-      setResolveClass(styles.green);
-    } else if (resolvedAddress && resolvedAddress !== account?.signer.address) {
-      setResolveString('crypto address belongs to another account, address: ' + getShortAddress(resolvedAddress));
-      setResolveClass(styles.red);
-    }
-    setCryptoAddressToResolve('');
-    setShowResolvingMessage(false);
-  }, [checkingCryptoAddress, resolvedAddress, account?.signer.address]);
-  
-
-  const cryptoAddressInfo = () => {
-    alert(
-      'Change your account address to an ENS name you own: in your ENS name page on ens.domains, click on "Records", "Edit Records", "Add record", add "plebbit-author-address" as record name, add your full address as value (you can copy it from your account data) and save.',
-    );
-  };
-
-  console.log('rerender', resolvedAddress, account?.signer.address, resolvedAddress === account?.signer.address)
-  console.log(cryptoAddressToResolve, cryptoAddress)
-
-  const saveCryptoAddress = async () => {
-    if (!cryptoAddress) {
-      alert('Please enter a crypto address.');
-      return;
-    } else if (resolvedAddress && resolvedAddress !== account?.signer.address) {
-      alert('Cannot save resolved crypto address, it belongs to another account, address: ' + resolvedAddress);
-      return;
-    }
-
-    try {
-      await setAccount({ ...account, author: { ...account?.author, address: cryptoAddress } });
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message);
-        console.log(error);
-      } else {
-        console.error('An unknown error occurred:', error);
-      }
-    }
-    setSavedCryptoAddress(true);
-    setCryptoAddressToResolve('');
-    setCryptoAddress('');
-    setCheckingCryptoAddress(false);
-  };
-
-  const checkCryptoAddress = () => {;
-    if (!cryptoAddress) {
-      alert('Please enter a crypto address.');
-      return;
-    }
-    setCryptoAddressToResolve(cryptoAddress);
-    setCheckingCryptoAddress(true);
-    setShowResolvingMessage(true);
-  }
-
-  useEffect(() => {
-    if (savedCryptoAddress) {
-      setTimeout(() => {
-        setSavedCryptoAddress(false);
-      }, 2000);
-    }
-  }, [savedCryptoAddress]);
-
-  return (
-    <span className={styles.categorySettings}>
-      <span className={styles.settingTitle}>display name</span>
-      <div className={styles.usernameInput}>
-        <input type='text' placeholder='My Name' value={username} onChange={(e) => setUsername(e.target.value)} />
-        <button className={styles.button} onClick={saveUsername}>
-          save
-        </button>
-        {savedUsername && <span className={styles.saved}>Saved.</span>}
-      </div>
-      <div className={styles.cryptoAddressSetting}>
-        <span className={styles.settingTitle}>crypto address</span>
-        <button className={styles.infoButton} onClick={cryptoAddressInfo}>
-          ?
-        </button>
-        <div className={styles.usernameInput}>
-          <input type='text' placeholder='address.eth' value={cryptoAddress} onChange={(e) => setCryptoAddress(e.target.value)} />
-          <button className={styles.button} onClick={saveCryptoAddress}>
-            save
-          </button>
-          {savedCryptoAddress && <span className={styles.saved}>Saved.</span>}
-        </div>
-        <div className={styles.checkCryptoAddress}>
-          <button className={styles.button} onClick={checkCryptoAddress}>
-            check
-          </button>{' '}
-          {checkingCryptoAddress ? <span className={resolveClass}>{resolveString}</span> : 'if the crypto address is resolved p2p'}
-        </div>
-        <div></div>
-      </div>
-    </span>
-  );
-};
+import ThemeSettings from './theme-settings';
+import LanguageSettings from './language-settings';
+import ProfileSettings from './profile-settings';
 
 const AccountSettings = () => {
   const account = useAccount();
@@ -334,15 +143,21 @@ const Settings = () => {
     <div className={styles.content}>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>interface language</span>
-        <LanguageSettings />
+        <span className={styles.categorySettings}>
+          <LanguageSettings />
+        </span>
       </div>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>theme</span>
-        <ThemeSettings />
+        <span className={styles.categorySettings}>
+          <ThemeSettings />
+        </span>
       </div>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>profile</span>
-        <ProfileSettings />
+        <span className={styles.categorySettings}>
+          <ProfileSettings />
+        </span>
       </div>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>account</span>
