7314db28bc795c458106780ee5f7ac884e33f6f4
plebeius.eth <mottoes_recap0u@icloud.com>
2023-12-21 13:05:43 +0100

    feat(profile): add functional sort by new or old

--- src/views/profile/profile.tsx
+++ src/views/profile/profile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { StateSnapshot, Virtuoso, VirtuosoHandle } from 'react-virtuoso';
 import { useAccount, useAccountComments, useAccountVotes, useComments } from '@plebbit/plebbit-react-hooks';
@@ -7,9 +7,63 @@ import styles from './profile.module.css';
 import AuthorSidebar from '../../components/author-sidebar';
 import Post from '../../components/post';
 import Reply from '../../components/reply';
-import SortDropdown from '../../components/sort-dropdown';
 
 const lastVirtuosoStates: { [key: string]: StateSnapshot } = {};
+
+const SortDropdown = ({ onSortChange }: { onSortChange: (sortType: string) => void }) => {
+  const sortLabels = ['new', 'old'];
+  const [selectedSort, setSelectedSort] = useState('new');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const dropdownChoicesRef = useRef<HTMLDivElement>(null);
+  const dropChoicesClass = isDropdownOpen ? styles.dropChoicesVisible : styles.dropChoicesHidden;
+
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node) &&
+      dropdownChoicesRef.current &&
+      !dropdownChoicesRef.current.contains(event.target as Node)
+    ) {
+      setIsDropdownOpen(false);
+    }
+  }, []);
+
+  const handleSortChange = (sortType: string) => {
+    setSelectedSort(sortType);
+    setIsDropdownOpen(false);
+    onSortChange(sortType);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
+  return (
+    <div className={styles.sortDropdown}>
+      <span className={styles.dropdownTitle}>sorted by: </span>
+      <div
+        className={styles.dropdown}
+        onClick={() => {
+          setIsDropdownOpen(!isDropdownOpen);
+        }}
+        ref={dropdownRef}
+      >
+        <span className={styles.selected}>{selectedSort}</span>
+      </div>
+      <div className={`${styles.dropChoices} ${dropChoicesClass}`} ref={dropdownChoicesRef}>
+        {sortLabels.map((filter: string, index: number) => (
+          <div key={index} className={styles.filter} onClick={() => handleSortChange(filter)}>
+            {filter}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Profile = () => {
   const account = useAccount();
@@ -24,29 +78,46 @@ const Profile = () => {
   const isSubmittedPage = isProfileSubmittedView(location.pathname);
   const isMobile = window.innerWidth < 768;
 
+  // get comments for upvoted/downvoted/comments/submitted pages
   const upvotedCommentCids = useMemo(() => accountVotes?.filter((vote) => vote.vote === 1).map((vote) => vote.commentCid) || [], [accountVotes]);
   const downvotedCommentCids = useMemo(() => accountVotes?.filter((vote) => vote.vote === -1).map((vote) => vote.commentCid) || [], [accountVotes]);
   const replyComments = useMemo(() => accountComments?.filter((comment) => comment.parentCid) || [], [accountComments]);
   const postComments = useMemo(() => accountComments?.filter((comment) => !comment.parentCid) || [], [accountComments]);
-
   const { comments: upvotedComments } = useComments({ commentCids: upvotedCommentCids });
   const { comments: downvotedComments } = useComments({ commentCids: downvotedCommentCids });
 
-  let virtuosoData;
+  // sort dropdown
+  const [sortType, setSortType] = useState('new');
+  const handleSortChange = (newSortType: string) => {
+    setSortType(newSortType);
+  };
+  let comments: any[] = [];
   if (isUpvotedPage) {
-    virtuosoData = upvotedComments;
+    comments = upvotedComments;
   } else if (isDownvotedPage) {
-    virtuosoData = downvotedComments;
+    comments = downvotedComments;
   } else if (isCommentsPage) {
-    virtuosoData = replyComments;
+    comments = replyComments;
   } else if (isSubmittedPage) {
-    virtuosoData = postComments;
+    comments = postComments;
   } else {
-    virtuosoData = accountComments;
+    comments = accountComments;
   }
+  const virtuosoData = useMemo(() => {
+    let sortedData = [...comments];
 
+    if (sortType === 'new') {
+      sortedData.sort((a, b) => b.timestamp - a.timestamp);
+    } else {
+      sortedData.sort((a, b) => a.timestamp - b.timestamp);
+    }
+
+    return sortedData;
+  }, [sortType, accountComments]);
+
+  // save last virtuoso state on each scroll
   const virtuosoRef = useRef<VirtuosoHandle | null>(null);
-
+  const lastVirtuosoState = lastVirtuosoStates?.[account?.shortAddress + params.sortType];
   useEffect(() => {
     const setLastVirtuosoState = () =>
       virtuosoRef.current?.getState((snapshot: StateSnapshot) => {
@@ -58,14 +129,12 @@ const Profile = () => {
     return () => window.removeEventListener('scroll', setLastVirtuosoState);
   }, [account?.shortAddress, params.sortType]);
 
-  const lastVirtuosoState = lastVirtuosoStates?.[account?.shortAddress + params.sortType];
-
   return (
     <div className={styles.content}>
       <div className={isMobile ? styles.sidebarMobile : styles.sidebarDesktop}>
         <AuthorSidebar />
       </div>
-      <SortDropdown />
+      <SortDropdown onSortChange={handleSortChange} />
       {account && !accountComments.length ? (
         'no posts'
       ) : (
