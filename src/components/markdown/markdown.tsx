361a479111098f592e4ffd214d950f31fd69b9be
plebeius.eth <mottoes_recap0u@icloud.com>
2024-01-23 11:19:50 +0100

    fix(markdown): use regex and a custom remark plugin to correctly style quotes and lists

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -1,12 +1,53 @@
-import ReactMarkdown from 'react-markdown';
-import rehypeSanitize from 'rehype-sanitize';
+import { useMemo } from 'react';
 import styles from './markdown.module.css';
+import ReactMarkdown from 'react-markdown';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
+import breaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 
 const Markdown = ({ content }: { content: string }) => {
+  // replace \n with \n\n when it follows a sentence starting with '>'
+  let preserveNewlineAfterQuote = content?.replace(/(>.*?)(\n)/g, '\n$1\n\n');
+
+  const customSchema = useMemo(
+    () => ({
+      ...defaultSchema,
+      tagNames: [...(defaultSchema.tagNames || []), 'div'],
+      attributes: {
+        ...defaultSchema.attributes,
+        div: ['className'],
+      },
+    }),
+    [],
+  );
+
+  const excludeBlockquote = () => (tree: any) => {
+    tree.children.forEach((node: any) => {
+      if (node.type === 'blockquote') {
+        node.type = 'div';
+        node.data = {
+          hName: 'div',
+          hProperties: {
+            className: styles.quote,
+          },
+        };
+      }
+    });
+  };
+
   return (
     <span className={styles.markdown}>
-      <ReactMarkdown children={content} remarkPlugins={[[remarkGfm, { singleTilde: false }]]} rehypePlugins={[rehypeSanitize]} />
+      <ReactMarkdown
+        children={preserveNewlineAfterQuote}
+        remarkPlugins={[excludeBlockquote, [remarkGfm, { singleTilde: false }], breaks]}
+        rehypePlugins={[[rehypeSanitize, customSchema]]}
+        components={{
+          br: () => <span>{''}</span>,
+          img: ({ src }) => <span>{src}</span>,
+          video: ({ src }) => <span>{src}</span>,
+          iframe: ({ src }) => <span>{src}</span>,
+        }}
+      />
     </span>
   );
 };
