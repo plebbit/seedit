a593923f17e0f6cbd59fa8f82ad1ea6723dbd24f
plebeius.eth <mottoes_recap0u@icloud.com>
2024-01-29 17:47:09 +0100

    style(subplebbit settings): hide read-only settings that are undefined

--- src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
+++ src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
@@ -72,7 +72,7 @@ const Title = ({ isReadOnly }: { isReadOnly: boolean }) => {
   const { title, setSubmitStore } = useSubplebbitSettingsStore();
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && !title ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('title')}</div>
       <div className={styles.boxSubtitle}>e.g., books: made from trees or pixels. recommendations, news, or thoughts</div>
       <div className={styles.boxInput}>
@@ -87,7 +87,7 @@ const Description = ({ isReadOnly }: { isReadOnly: boolean }) => {
   const { description, setSubmitStore } = useSubplebbitSettingsStore();
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && !description ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('description')}</div>
       <div className={styles.boxSubtitle}>shown in the sidebar of your community</div>
       <div className={styles.boxInput}>
@@ -129,7 +129,7 @@ const Logo = ({ isReadOnly }: { isReadOnly: boolean }) => {
   }, [suggested?.avatarUrl]);
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && !logoUrl ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('logo')}</div>
       <div className={styles.boxSubtitle}>set a community logo using its direct image link (ending in .jpg, .png)</div>
       <div className={styles.boxInput}>
@@ -187,7 +187,7 @@ const Rules = ({ isReadOnly }: { isReadOnly: boolean }) => {
   };
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && (!rules || rules.length < 1) ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('rules')}</div>
       <div className={styles.boxSubtitle}>shown in the sidebar of your community</div>
       <div className={styles.boxInput}>
@@ -256,7 +256,7 @@ const Moderators = ({ isReadOnly }: { isReadOnly: boolean }) => {
   };
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && !roles ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('moderators')}</div>
       <div className={styles.boxSubtitle}>let other users moderate and post without challenges</div>
       <div className={styles.boxInput}>
@@ -643,7 +643,7 @@ const Challenges = ({ isReadOnly, readOnlyChallenges }: { isReadOnly: boolean; r
   };
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isReadOnly && !challenges ? styles.hidden : styles.visible}`}>
       <div className={styles.boxTitle}>{t('challenges')}</div>
       <div className={styles.boxSubtitle}>choose one or more challenges to prevent spam</div>
       <div className={styles.boxInput}>
@@ -709,7 +709,7 @@ const JSONSettings = ({ isReadOnly }: { isReadOnly: boolean }) => {
   };
 
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box}`}>
       <div className={`${styles.boxTitle} ${styles.JSONSettingsTitle}`}>JSON settings</div>
       <div className={styles.boxSubtitle}>quickly copy or paste the community settings</div>
       <div className={`${styles.boxInput} ${styles.JSONSettings}`}>
