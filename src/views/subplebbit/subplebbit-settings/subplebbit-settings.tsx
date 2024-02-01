66faf764c2ee85ab4368b33d259f7f5ebad62909
plebeius.eth <mottoes_recap0u@icloud.com>
2024-02-01 20:25:51 +0100

    style(subplebbit settings): clearer info text for exclude settings

--- src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
+++ src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
@@ -323,7 +323,7 @@ interface ChallengeSettingsProps {
 
 const rolesToExclude = ['moderator', 'admin', 'owner'];
 const actionsToExclude: Array<'post' | 'reply' | 'vote'> = ['post', 'reply', 'vote'];
-const nonActionsToExclude: Array<'non-post' | 'non-reply' | 'non-vote'> = ['non-post', 'non-reply', 'non-vote'];
+const nonActionsToExclude: Array<'not post' | 'not reply' | 'not vote'> = ['not post', 'not reply', 'not vote'];
 
 const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettingsStore, settings, showSettings }: ChallengeSettingsProps) => {
   const { name, options } = challenge || {};
@@ -355,7 +355,7 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
     setShowExcludeSettings(newShowExcludeSettings);
   };
 
-  const handleExcludeChange = (excludeIndex: number, type: keyof Exclude | 'non-post' | 'non-reply' | 'non-vote', value: any) => {
+  const handleExcludeChange = (excludeIndex: number, type: keyof Exclude | 'not post' | 'not reply' | 'not vote', value: any) => {
     const updatedChallenges = settings.challenges.map((ch: any, idx: number) => {
       if (idx === index) {
         const updatedExclude = ch.exclude.map((ex: any, exIdx: number) => {
@@ -366,13 +366,13 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
             const processedValue = value === '' ? undefined : value;
 
             switch (type) {
-              case 'non-post':
+              case 'not post':
                 newEx.post = value ? undefined : false;
                 break;
-              case 'non-reply':
+              case 'not reply':
                 newEx.reply = value ? undefined : false;
                 break;
-              case 'non-vote':
+              case 'not vote':
                 newEx.vote = value ? undefined : false;
                 break;
               case 'role':
@@ -456,8 +456,8 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
               <>
                 {isReadOnly && !exclude?.address ? null : (
                   <div className={styles.challengeOption}>
-                    Users
-                    <div className={styles.challengeOptionDescription}>Exclude specific users by their addresses, separated by a comma</div>
+                    User's address
+                    <div className={styles.challengeOptionDescription}>Is one of the following:</div>
                     {isReadOnly ? (
                       <span>{exclude?.address?.join(', ')}</span>
                     ) : (
@@ -472,10 +472,10 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                 )}
                 {isReadOnly && !exclude?.postScore && !exclude?.postReply ? null : (
                   <div className={styles.challengeOption}>
-                    Users with karma
+                    User's karma
                     {isReadOnly && !exclude?.postScore ? null : (
                       <>
-                        <div className={styles.challengeOptionDescription}>Minimum post karma required:</div>
+                        <div className={styles.challengeOptionDescription}>Post karma is at least:</div>
                         {isReadOnly ? (
                           <span>{exclude?.postScore}</span>
                         ) : (
@@ -485,7 +485,7 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                     )}
                     {isReadOnly && !exclude?.postReply ? null : (
                       <>
-                        <div className={styles.challengeOptionDescription}>Minimum comment karma required:</div>
+                        <div className={styles.challengeOptionDescription}>Comment karma is at least:</div>
                         {isReadOnly ? (
                           <span>{exclude?.postReply}</span>
                         ) : (
@@ -497,8 +497,8 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                 )}
                 {isReadOnly && !exclude?.firstCommentTimestamp ? null : (
                   <div className={styles.challengeOption}>
-                    Users by account age
-                    <div className={styles.challengeOptionDescription}>Minimum account age in seconds (eg. 86400 = 24h):</div>
+                    User's account age
+                    <div className={styles.challengeOptionDescription}>In seconds (eg. 86400 = 24h) is at least:</div>
                     {isReadOnly ? (
                       <span>{exclude?.firstCommentTimestamp}</span>
                     ) : (
@@ -512,8 +512,8 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                 )}
                 {isReadOnly && !exclude?.role ? null : (
                   <div className={styles.challengeOption}>
-                    Moderators
-                    <div className={styles.challengeOptionDescription}>Exclude a specific moderator role</div>
+                    User's role
+                    <div className={styles.challengeOptionDescription}>Exclude a specific assigned role</div>
                     {rolesToExclude.map((role) =>
                       isReadOnly && !exclude?.role?.includes(role) ? null : (
                         <div key={role}>
@@ -527,7 +527,7 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                                 onChange={() => handleExcludeChange(excludeIndex, 'role', role)}
                                 disabled={isReadOnly}
                               />
-                              exclude {role}
+                              is {role}
                             </label>
                           )}
                         </div>
@@ -537,7 +537,7 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                 )}
                 {isReadOnly && actionsToExclude.some((action) => exclude.hasOwnProperty(action)) ? null : (
                   <div className={styles.challengeOption}>
-                    Actions
+                    User's action
                     <div className={styles.challengeOptionDescription}>Exclude a specific user action</div>
                     {actionsToExclude.map((action) =>
                       isReadOnly && !exclude?.[action] ? null : (
@@ -552,14 +552,14 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                                 onChange={(e) => handleExcludeChange(excludeIndex, action, e.target.checked)}
                                 disabled={isReadOnly}
                               />
-                              exclude {action}
+                              is {action}
                             </label>
                           )}
                         </div>
                       ),
                     )}
                     {nonActionsToExclude.map((nonAction) =>
-                      isReadOnly && exclude?.[nonAction.replace('non-', '')] ? null : (
+                      isReadOnly && exclude?.[nonAction.replace('not ', '')] ? null : (
                         <div key={nonAction}>
                           {isReadOnly ? (
                             <span className={styles.readOnlyActionExclude}>{nonAction} excluded</span>
@@ -567,11 +567,11 @@ const ChallengeSettings = ({ challenge, index, isReadOnly, setSubplebbitSettings
                             <label>
                               <input
                                 type='checkbox'
-                                checked={exclude?.[nonAction.replace('non-', '')] === undefined}
+                                checked={exclude?.[nonAction.replace('not ', '')] === undefined}
                                 onChange={(e) => handleExcludeChange(excludeIndex, nonAction, e.target.checked)}
                                 disabled={isReadOnly}
                               />
-                              exclude {nonAction}
+                              is {nonAction}
                             </label>
                           )}
                         </div>
