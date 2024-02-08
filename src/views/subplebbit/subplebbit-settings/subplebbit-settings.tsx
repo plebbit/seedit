65fd5af25cfc1eb6e7c2b41737de8c0290fdfb66
plebeius.eth <mottoes_recap0u@icloud.com>
2024-02-08 14:48:50 +0100

    style(subplebbit settings): add section for delete community button

--- src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
+++ src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
@@ -901,16 +901,26 @@ const SubplebbitSettings = () => {
       <Challenges isReadOnly={isReadOnly} readOnlyChallenges={subplebbit?.challenges} />
       {!isInCreateSubplebbitView && <JSONSettings isReadOnly={isReadOnly} />}
       <div className={styles.saveOptions}>
+        {!isInCreateSubplebbitView && (
+          <div className={`${styles.box} ${styles.deleteCommunity}`}>
+            <div className={styles.boxTitle}>{t('delete_community')}</div>
+            <div className={styles.boxSubtitle}>{t('delete_community_description')}</div>
+            <div className={styles.boxInput}>
+              <div className={styles.deleteSubplebbit}>
+                <button onClick={_deleteSubplebbit} disabled={showDeleting || showSaving}>
+                  {t('delete')}
+                </button>
+                <span className={styles.deletingString}>{showDeleting && <LoadingEllipsis string={t('deleting')} />}</span>
+              </div>
+            </div>
+          </div>
+        )}
         {!isReadOnly && (
           <button onClick={isInCreateSubplebbitView ? _createSubplebbit : saveSubplebbit} disabled={showSaving || showDeleting}>
             {isInCreateSubplebbitView ? t('create_community') : t('save_options')}
           </button>
         )}
         {showSaving && <LoadingEllipsis string={t('saving')} />}
-        <button onClick={_deleteSubplebbit} disabled={showDeleting || showSaving}>
-          delete community
-        </button>
-        {showDeleting && <LoadingEllipsis string={t('deleting')} />}
       </div>
     </div>
   );
