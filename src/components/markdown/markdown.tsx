0edca2aad90821e1757ee451d53967eadb8552e2
plebeius.eth <mottoes_recap0u@icloud.com>
2024-02-13 16:09:40 +0100

    perf(markdown): only render links in content if its length is less than 10000, to avoid lag

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -4,12 +4,24 @@ import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
 import supersub from 'remark-supersub';
 
-const Markdown = ({ content }: { content: string }) => {
+interface MarkdownProps {
+  content: string;
+}
+
+const MAX_LENGTH_FOR_GFM = 10000; // remarkGfm lags with large content
+
+const Markdown = ({ content }: MarkdownProps) => {
+  const remarkPlugins: any[] = [[supersub]];
+
+  if (content.length <= MAX_LENGTH_FOR_GFM) {
+    remarkPlugins.push([remarkGfm, { singleTilde: false }]);
+  }
+
   return (
     <span className={styles.markdown}>
       <ReactMarkdown
         children={content}
-        remarkPlugins={[[remarkGfm, { singleTilde: false }], supersub]}
+        remarkPlugins={remarkPlugins}
         rehypePlugins={[[rehypeSanitize]]}
         components={{
           img: ({ src }) => <span>{src}</span>,
