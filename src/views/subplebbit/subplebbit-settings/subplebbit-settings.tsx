57c5903508bfdf0dcb57814064605aab187a850f
plebeius.eth <mottoes_recap0u@icloud.com>
2024-03-02 21:21:38 +0100

    perf(subplebbit settings): remove unnecessary useEffect

--- src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
+++ src/views/subplebbit/subplebbit-settings/subplebbit-settings.tsx
@@ -773,12 +773,7 @@ const Challenges = ({ isReadOnly, readOnlyChallenges }: { isReadOnly: boolean; r
 const JSONSettings = ({ isReadOnly = false }: { isReadOnly?: boolean }) => {
   const { t } = useTranslation();
   const { challenges, title, description, address, suggested, rules, roles, settings, subplebbitAddress, setSubplebbitSettingsStore } = useSubplebbitSettingsStore();
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    const JSONSettings = JSON.stringify({ title, description, address, suggested, rules, roles, settings, challenges, subplebbitAddress }, null, 2);
-    setText(JSONSettings);
-  }, [challenges, title, description, address, suggested, rules, roles, settings, subplebbitAddress]);
+  const [text, setText] = useState(JSON.stringify({ title, description, address, suggested, rules, roles, settings, challenges, subplebbitAddress }, null, 2));
 
   const handleChange = (newText: string) => {
     setText(newText);
