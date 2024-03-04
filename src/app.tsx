365b40d3626a40131b7b0d722a3d9e0e6ab9359b
plebeius.eth <mottoes_recap0u@icloud.com>
2024-03-04 19:13:36 +0100

    feat(app): redirect to link with hash on first load

--- src/app.tsx
+++ src/app.tsx
@@ -24,6 +24,7 @@ import StickyHeader from './components/sticky-header';
 import TopBar from './components/topbar';
 
 export const sortTypes = ['hot', 'new', 'active', 'controversialAll', 'topAll'];
+const isElectron = window.isElectron === true;
 
 const CheckRouteParams = () => {
   let { sortType, timeFilterName, accountCommentIndex } = useParams<{ sortType?: string; timeFilterName?: string; accountCommentIndex?: string }>();
@@ -70,6 +71,15 @@ const App = () => {
       <Outlet />
     </>
   );
+
+  useEffect(() => {
+    if (!window.location.hash && !isElectron) {
+      // Calculate the base URL (protocol + host)
+      const baseURL = window.location.protocol + '//' + window.location.host + '/';
+      // Redirect to the base URL with `#/`
+      window.location.replace(baseURL + '#/');
+    }
+  }, []);
 
   return (
     <div className={`${styles.app} ${theme}`}>
