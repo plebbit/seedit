22a5197ad03bab1812e9a49fa90ad574988c6f45
plebeius.eth <mottoes_recap0u@icloud.com>
2024-03-23 21:53:48 +0100

    chore(package.json): remove gifff to use directly

--- src/hooks/use-fetch-gif-first-frame.ts
+++ src/hooks/use-fetch-gif-first-frame.ts
@@ -1,5 +1,53 @@
 import { useEffect, useState } from 'react';
-import gifff from 'gifff/canvas';
+
+export const fetchImage = (url: string): Promise<ArrayBuffer> => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onloadend = () => {
+      if (request.response !== undefined && (request.status === 200 || request.status === 304)) {
+        resolve(request.response);
+      } else {
+        reject(new Error(`XMLHttpRequest, ${request.statusText}`));
+      }
+    };
+    request.send();
+  });
+};
+
+export const readImage = (file: File): Promise<ArrayBuffer> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result as ArrayBuffer);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+};
+
+const parseGif = async (buf: ArrayBuffer): Promise<Blob> => {
+  const image = new Image();
+  await new Promise((resolve) => {
+    image.src = URL.createObjectURL(new Blob([buf]));
+    image.onload = resolve;
+  });
+  const canvas = document.createElement('canvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+  const ctx = canvas.getContext('2d');
+  if (ctx === null) throw new Error('Canvas Context null');
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+  return await new Promise((resolve, reject) =>
+    canvas.toBlob((blob) => {
+      if (blob === null) {
+        reject('Canvas Blob null');
+      } else {
+        resolve(blob);
+      }
+    }),
+  );
+};
 
 const useFetchGifFirstFrame = (url: string | undefined) => {
   const [frameUrl, setFrameUrl] = useState<string | null>(null);
@@ -14,7 +62,7 @@ const useFetchGifFirstFrame = (url: string | undefined) => {
 
     const fetchFrame = async () => {
       try {
-        const blob = await gifff(url);
+        const blob = typeof url === 'string' ? await parseGif(await fetchImage(url)) : await parseGif(await readImage(url as File));
         const objectUrl = URL.createObjectURL(blob);
         if (isActive) setFrameUrl(objectUrl);
       } catch (error) {
