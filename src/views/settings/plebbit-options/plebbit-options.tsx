5ab14bff0a20757bcb55deb019aa6aebe0dfade8
plebeius.eth <mottoes_recap0u@icloud.com>
2024-04-22 22:19:23 +0200

    feat(settings): add plebbit options

--- src/views/settings/plebbit-options/plebbit-options.tsx
+++ src/views/settings/plebbit-options/plebbit-options.tsx
@@ -1,63 +1,142 @@
+import { useState } from 'react';
+import { useAccount } from '@plebbit/plebbit-react-hooks';
 import styles from './plebbit-options.module.css';
 import { useTranslation } from 'react-i18next';
 
-const PlebbitRPCSettings = () => {
-  const { t } = useTranslation();
+const isElectron = window.isElectron === true;
 
-  return <div className={styles.plebbitRPCSettings}></div>;
-};
-
-const IPFSGatewaysSettings = () => {
+const IPFSGatewaysSettings = ({ ipfsGatewayUrls, mediaIpfsGatewayUrl }: { ipfsGatewayUrls: any; mediaIpfsGatewayUrl: any }) => {
   const { t } = useTranslation();
+  const ipfsGatewayUrlsDefaultValue = ipfsGatewayUrls?.join('\n');
 
   return (
     <div className={styles.ipfsGatewaysSettings}>
-      <textarea className={styles.input} />
-      <div></div>
-      <span className={styles.settingTitle}>profile pics gateway</span>
+      <div className={styles.ipfsGatewaysSetting}>
+        <textarea defaultValue={ipfsGatewayUrlsDefaultValue} />
+        <button>{t('save')}</button>
+      </div>
+      <span className={styles.settingTitle}>nft profile pics gateway</span>
+      <div>
+        <input type='text' defaultValue={mediaIpfsGatewayUrl} />
+      </div>
     </div>
   );
 };
 
-const PubsubProvidersSettings = () => {
+const PubsubProvidersSettings = ({ pubsubHttpClientsOptions }: { pubsubHttpClientsOptions: any }) => {
   const { t } = useTranslation();
+  const pubsubProvidersDefaultValue = pubsubHttpClientsOptions?.join('\n');
 
-  return <div className={styles.pubsubProvidersSettings}>test</div>;
+  return (
+    <div className={styles.pubsubProvidersSettings}>
+      <textarea defaultValue={pubsubProvidersDefaultValue} />
+      <button>{t('save')}</button>
+    </div>
+  );
 };
 
-const BlockchainProvidersSettings = () => {
+const BlockchainProvidersSettings = ({ chainProviders }: { chainProviders: any }) => {
   const { t } = useTranslation();
+  const ethRpcDefaultValue = chainProviders?.['eth']?.urls.join(', ');
+  const solRpcDefaultValue = chainProviders?.['sol']?.urls.join(', ');
+  const maticRpcDefaultValue = chainProviders?.['matic']?.urls.join(', ');
 
-  return <div className={styles.blockchainProvidersSettings}>test</div>;
+  return (
+    <div className={styles.blockchainProvidersSettings}>
+      <span className={styles.settingTitle}>ethereum rpc, for .eth addresses</span>
+      <div>
+        <input type='text' defaultValue={ethRpcDefaultValue} />
+        <button>{t('save')}</button>
+      </div>
+      <span className={styles.settingTitle}>solana rpc, for .sol addresses</span>
+      <div>
+        <input type='text' defaultValue={solRpcDefaultValue} />
+      </div>
+      <span className={styles.settingTitle}>polygon rpc, for nft profile pics</span>
+      <div>
+        <input type='text' defaultValue={maticRpcDefaultValue} />
+      </div>
+    </div>
+  );
+};
+
+const PlebbitRPCSettings = ({ isElectron }: { isElectron: boolean }) => {
+  const { t } = useTranslation();
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <div className={styles.plebbitRPCSettings}>
+      <div>
+        <input type='text' placeholder='' disabled={!isElectron} />
+        <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'X' : '?'}</button>
+        <button disabled={!isElectron}>{t('save')}</button>
+      </div>
+      {showInfo && (
+        <div className={styles.plebbitRpcSettingsInfo}>
+          connect to a plebbit full node locally, or remotely if SSL is setup
+          <br />
+          <ol>
+            <li>get secret auth key from the node</li>
+            <li>get IP address and port used by the node</li>
+            <li>
+              save node rpc: <code>{`ws://<IP>:<port>/<secretAuthKey>`}</code>
+            </li>
+          </ol>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const NodeDataPathSettings = ({ isElectron }: { isElectron: boolean }) => {
+  const { t } = useTranslation();
+  const path = '~/Application Support/seedit';
+
+  return (
+    <div className={styles.nodeDataPathSettings}>
+      <div>
+        <input type='text' placeholder={isElectron ? path : ''} disabled={!isElectron} />
+        <button disabled={!isElectron}>{t('save')}</button>
+      </div>
+    </div>
+  );
 };
 
 const PlebbitOptions = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
+  const { plebbitOptions, mediaIpfsGatewayUrl } = useAccount() || {};
+  const { ipfsGatewayUrls, pubsubHttpClientsOptions, chainProviders } = plebbitOptions || {};
 
   return (
     <div className={styles.content}>
       <div className={styles.category}>
-        <span className={styles.categoryTitle}>plebbit rpc</span>
-        <span className={styles.categorySettings}>
-          <PlebbitRPCSettings />
-        </span>
-      </div>
-      <div className={styles.category}>
         <span className={styles.categoryTitle}>ipfs gateways</span>
         <span className={styles.categorySettings}>
-          <IPFSGatewaysSettings />
+          <IPFSGatewaysSettings ipfsGatewayUrls={ipfsGatewayUrls} mediaIpfsGatewayUrl={mediaIpfsGatewayUrl} />
         </span>
       </div>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>pubsub providers</span>
         <span className={styles.categorySettings}>
-          <PubsubProvidersSettings />
+          <PubsubProvidersSettings pubsubHttpClientsOptions={pubsubHttpClientsOptions} />
         </span>
       </div>
       <div className={styles.category}>
         <span className={styles.categoryTitle}>blockchain providers</span>
         <span className={styles.categorySettings}>
-          <BlockchainProvidersSettings />
+          <BlockchainProvidersSettings chainProviders={chainProviders} />
+        </span>
+      </div>
+      <div className={styles.category}>
+        <span className={styles.categoryTitle}>node rpc</span>
+        <span className={styles.categorySettings}>
+          <PlebbitRPCSettings isElectron={isElectron} />
+        </span>
+      </div>
+      <div className={styles.category}>
+        <span className={styles.categoryTitle}>node data path</span>
+        <span className={styles.categorySettings}>
+          <NodeDataPathSettings isElectron={isElectron} />
         </span>
       </div>
     </div>
