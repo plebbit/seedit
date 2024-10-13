7d0d00168082d14dd9c1b735dbd86c31d8fff889
Tom (plebeius.eth) <tom@plebbit.com>
2024-10-13 18:29:53 +0200

    add isAndroid flag

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,12 @@ const config: CapacitorConfig = {
   appId: 'seedit.android',
   appName: 'seedit',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    Device: {
+      lazyLoad: true,
+    },
+  },
 };
 
 export default config;
