9ff01c33c6fa04c5d9c70e7c37aa5b035a8cd8bb
Tom (plebeius.eth) <tom@plebbit.com>
2024-12-15 16:17:58 +0100

    perf(media): fix thumbnails getting stuck due to incorrect memoization

--- src/hooks/use-comment-media-info.ts
+++ src/hooks/use-comment-media-info.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { Comment } from '@plebbit/plebbit-react-hooks';
 import { getCommentMediaInfo, fetchWebpageThumbnailIfNeeded } from '../lib/utils/media-utils';
@@ -9,21 +9,23 @@ export const useCommentMediaInfo = (comment: Comment) => {
   const params = useParams();
   const isInPostPageView = isPostPageView(location.pathname, params);
   const isInPendingPostView = isPendingPostView(location.pathname, params);
-  // some sites have CORS access, so the thumbnail can be fetched client-side, which is helpful if subplebbit.settings.fetchThumbnailUrls is false
-  const initialCommentMediaInfo = useMemo(() => getCommentMediaInfo(comment), [comment]);
-  const [commentMediaInfo, setCommentMediaInfo] = useState(initialCommentMediaInfo);
-  const fetchThumbnail = useCallback(async () => {
-    if (!isInPostPageView && !isInPendingPostView) {
-      return; // don't fetch in feed view, it displaces the posts
-    }
-    if (initialCommentMediaInfo?.type === 'webpage' && !initialCommentMediaInfo.thumbnail) {
-      const newMediaInfo = await fetchWebpageThumbnailIfNeeded(initialCommentMediaInfo);
-      setCommentMediaInfo(newMediaInfo);
-    }
-  }, [initialCommentMediaInfo, isInPostPageView, isInPendingPostView]);
-  useEffect(() => {
-    fetchThumbnail();
-  }, [fetchThumbnail]);
 
-  return commentMediaInfo;
+  // some sites have CORS access, so the thumbnail can be fetched client-side, which is helpful if subplebbit.settings.fetchThumbnailUrls is false
+  const fetchThumbnail = useCallback(async () => {
+    let commentMediaInfo = getCommentMediaInfo(comment);
+    if (commentMediaInfo?.type === 'webpage' && !commentMediaInfo.thumbnail) {
+      const newMediaInfo = await fetchWebpageThumbnailIfNeeded(commentMediaInfo);
+      commentMediaInfo = newMediaInfo;
+    }
+    return commentMediaInfo;
+  }, [comment]);
+
+  useEffect(() => {
+    // don't fetch in feed view, it displaces the posts
+    if (isInPostPageView || isInPendingPostView) {
+      fetchThumbnail();
+    }
+  }, [fetchThumbnail, isInPostPageView, isInPendingPostView]);
+
+  return getCommentMediaInfo(comment);
 };
