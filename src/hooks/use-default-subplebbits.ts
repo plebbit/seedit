533b6ec30b1353a2bd8562ee2a68389af133a787
Tom (plebeius.eth) <tom@plebbit.com>
2025-02-06 11:33:59 +0100

    fix(subplebbits list): remove categories, subplebbits should be sorted by rank (via future pubsub voting)

--- src/hooks/use-default-subplebbits.ts
+++ src/hooks/use-default-subplebbits.ts
@@ -1,45 +1,38 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useAccount } from '@plebbit/plebbit-react-hooks';
-import Plebbit from '@plebbit/plebbit-js/dist/browser/index.js';
+import { Subplebbit, useAccount } from '@plebbit/plebbit-react-hooks';
 import useContentOptionsStore from '../stores/use-content-options-store';
 
-interface Subplebbit {
+export interface MultisubMetadata {
+  title: string;
+  description: string;
+  createdAt: number;
+  updatedAt: number;
+}
+
+export interface MultisubSubplebbit {
   title?: string;
   address: string;
   tags?: string[];
   features?: string[];
 }
 
-export interface SubplebbitWithDisplay extends Subplebbit {
-  displayAddress: string;
-}
-
-let cache: Subplebbit[] | null = null;
-
-export const categorizeSubplebbits = (subplebbits: Subplebbit[]) => {
-  const plebbitSubs = subplebbits.filter((sub) => sub.tags?.includes('plebbit'));
-  const interestsSubs = subplebbits.filter(
-    (sub) => sub.tags?.includes('topic') && !sub.tags?.includes('plebbit') && !sub.tags?.includes('country') && !sub.tags?.includes('international'),
-  );
-  const randomSubs = subplebbits.filter((sub) => sub.tags?.includes('random') && !sub.tags?.includes('plebbit'));
-  const internationalSubs = subplebbits.filter((sub) => sub.tags?.includes('international') || sub.tags?.includes('country'));
-  const projectsSubs = subplebbits.filter((sub) => sub.tags?.includes('project') && !sub.tags?.includes('plebbit') && !sub.tags?.includes('topic'));
-
-  return { plebbitSubs, interestsSubs, randomSubs, internationalSubs, projectsSubs };
-};
+let cacheSubplebbits: MultisubSubplebbit[] | null = null;
+let cacheMetadata: MultisubMetadata | null = null;
 
 export const useDefaultSubplebbits = () => {
-  const [subplebbits, setSubplebbits] = useState<Subplebbit[]>([]);
+  const [subplebbits, setSubplebbits] = useState<MultisubSubplebbit[]>([]);
 
   useEffect(() => {
-    if (cache) {
+    if (cacheSubplebbits) {
       return;
     }
     (async () => {
       try {
-        const multisub = await fetch('https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json').then((res) => res.json());
-        cache = multisub.subplebbits;
+        const multisub = await fetch(
+          'https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json',
+          // { cache: 'no-cache' }
+        ).then((res) => res.json());
+        cacheSubplebbits = multisub.subplebbits;
         setSubplebbits(multisub.subplebbits);
       } catch (e) {
         console.warn(e);
@@ -47,7 +40,7 @@ export const useDefaultSubplebbits = () => {
     })();
   }, []);
 
-  return cache || subplebbits;
+  return cacheSubplebbits || subplebbits;
 };
 
 export const useDefaultSubplebbitAddresses = () => {
@@ -65,19 +58,7 @@ export const useDefaultSubplebbitAddresses = () => {
     });
   }, [defaultSubplebbits, hideAdultCommunities, hideGoreCommunities, hideAntiCommunities, hideVulgarCommunities]);
 
-  const categorizedSubplebbits = useMemo(() => categorizeSubplebbits(filteredSubplebbits), [filteredSubplebbits]);
-
-  return useMemo(
-    () =>
-      [
-        ...categorizedSubplebbits.plebbitSubs,
-        ...categorizedSubplebbits.interestsSubs,
-        ...categorizedSubplebbits.randomSubs,
-        ...categorizedSubplebbits.projectsSubs,
-        ...categorizedSubplebbits.internationalSubs,
-      ].map((subplebbit) => subplebbit.address),
-    [categorizedSubplebbits],
-  );
+  return useMemo(() => filteredSubplebbits.map((subplebbit) => subplebbit.address), [filteredSubplebbits]);
 };
 
 export const useDefaultAndSubscriptionsSubplebbitAddresses = () => {
@@ -96,48 +77,28 @@ export const useDefaultAndSubscriptionsSubplebbitAddresses = () => {
   }, [subscriptions, defaultSubplebbitAddresses]);
 };
 
-export const useDefaultAndSubscriptionsSubplebbits = (): SubplebbitWithDisplay[] => {
-  const account = useAccount();
-  const { subplebbitAddress: subplebbitAddressParam } = useParams();
-  const defaultSubplebbits = useDefaultSubplebbits();
+export const useMultisubMetadata = () => {
+  const [metadata, setMetadata] = useState<MultisubMetadata | null>(null);
 
-  return useMemo(() => {
-    const subplebbitsObj: { [key: string]: SubplebbitWithDisplay } = {};
-
-    const addSubplebbit = (subplebbit: Subplebbit) => {
-      let displayAddress = subplebbit.address.includes('.') ? subplebbit.address : Plebbit.getShortAddress(subplebbit.address);
-
-      // Append title in parentheses only if the address doesn't contain '.'
-      if (!subplebbit.address.includes('.') && subplebbit.title) {
-        displayAddress += ` (${subplebbit.title})`;
-      }
-
-      if (displayAddress.length > 40) {
-        displayAddress = displayAddress.substring(0, 37) + '...';
-      }
-
-      subplebbitsObj[subplebbit.address] = { ...subplebbit, displayAddress };
-    };
-
-    // Add default subplebbits
-    defaultSubplebbits.forEach((subplebbit) => {
-      if (subplebbit.address) {
-        addSubplebbit(subplebbit);
-      }
-    });
-
-    // Add subscribed subplebbits
-    account?.subscriptions?.forEach((address: string) => {
-      if (!subplebbitsObj[address]) {
-        addSubplebbit({ address });
-      }
-    });
-
-    // Add the current subplebbit if not already in the list
-    if (subplebbitAddressParam && !subplebbitsObj[subplebbitAddressParam]) {
-      addSubplebbit({ address: subplebbitAddressParam });
+  useEffect(() => {
+    if (cacheMetadata) {
+      return;
     }
+    (async () => {
+      try {
+        const multisub = await fetch(
+          'https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json',
+          // { cache: 'no-cache' }
+        ).then((res) => res.json());
+        const { title, description, createdAt, updatedAt } = multisub;
+        const metadata: MultisubMetadata = { title, description, createdAt, updatedAt };
+        cacheMetadata = metadata;
+        setMetadata(metadata);
+      } catch (e) {
+        console.warn(e);
+      }
+    })();
+  }, []);
 
-    return Object.values(subplebbitsObj);
-  }, [account?.subscriptions, defaultSubplebbits, subplebbitAddressParam]);
+  return cacheMetadata || metadata;
 };
