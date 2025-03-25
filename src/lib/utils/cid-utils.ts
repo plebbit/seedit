5aecc9f41e34902ff4a2536b007be8b0abbf3054
Tom (plebeius.eth) <tom@plebbit.com>
2025-03-25 14:50:18 +0100

    fix potential undefined

--- src/lib/utils/cid-utils.ts
+++ src/lib/utils/cid-utils.ts
@@ -1,16 +1,16 @@
 import { Comment } from '@plebbit/plebbit-react-hooks';
 
 export const findTopParentCidOfReply = (replyCid: string, post: Comment): string | null => {
-  if (!post.replyCount || post.replyCount === 0) {
+  if (!post?.replyCount || post?.replyCount === 0) {
     return null;
   }
 
-  for (const firstLevelReply of post.replies?.pages?.topAll?.comments) {
-    if (firstLevelReply.cid === replyCid) {
-      return firstLevelReply.cid;
+  for (const firstLevelReply of post?.replies?.pages?.topAll?.comments) {
+    if (firstLevelReply?.cid === replyCid) {
+      return firstLevelReply?.cid;
     }
 
-    const result = findInDeeperReplies(replyCid, firstLevelReply, firstLevelReply.cid);
+    const result = findInDeeperReplies(replyCid, firstLevelReply, firstLevelReply?.cid);
     if (result) {
       return result;
     }
@@ -20,8 +20,8 @@ export const findTopParentCidOfReply = (replyCid: string, post: Comment): string
 };
 
 const findInDeeperReplies = (replyCid: string, currentReply: Comment, firstLevelParentCid: string): string | null => {
-  if (currentReply.replyCount > 0 && currentReply.replies?.pages?.topAll?.comments) {
-    for (const deeperReply of currentReply.replies.pages.topAll.comments) {
+  if (currentReply?.replyCount > 0 && currentReply?.replies?.pages?.topAll?.comments) {
+    for (const deeperReply of currentReply?.replies.pages.topAll.comments) {
       if (deeperReply.cid === replyCid) {
         return firstLevelParentCid;
       }
