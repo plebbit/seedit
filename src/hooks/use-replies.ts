6f56b66e66a83af24d5acb738bb7c7c11076c1fd
Tom (plebeius.eth) <tom@plebbit.com>
2025-05-06 16:31:12 +0200

    Update use-replies.ts

--- src/hooks/use-replies.ts
+++ src/hooks/use-replies.ts
@@ -8,20 +8,20 @@ const useRepliesAndAccountReplies = (comment: Comment) => {
 
   // the account's replies have a delay before getting published, so get them locally from accountComments instead
   const accountRepliesNotYetPublished = useMemo(() => {
-    const replies = comment?.replies?.pages?.topAll?.comments || [];
+    const replies = comment?.replies?.pages?.best?.comments || [];
     const replyCids = new Set(replies.map((reply: Comment) => reply?.cid));
     // filter out the account comments already in comment.replies, so they don't appear twice
     return accountComments.filter((accountReply) => !replyCids.has(accountReply?.cid));
-  }, [comment?.replies?.pages?.topAll?.comments, accountComments]);
+  }, [comment?.replies?.pages?.best?.comments, accountComments]);
 
   const repliesAndNotYetPublishedReplies = useMemo(() => {
     return [
       // put the author's unpublished replies at the top, latest first (reverse)
       ...accountRepliesNotYetPublished.reverse(),
       // put the published replies after,
-      ...(comment?.replies?.pages?.topAll?.comments || []),
+      ...(comment?.replies?.pages?.best?.comments || []),
     ];
-  }, [comment?.replies?.pages?.topAll?.comments, accountRepliesNotYetPublished]);
+  }, [comment?.replies?.pages?.best?.comments, accountRepliesNotYetPublished]);
 
   return repliesAndNotYetPublishedReplies;
 };
