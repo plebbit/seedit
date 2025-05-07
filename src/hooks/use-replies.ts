32ddeddd486fdd0b8ac451a2d1804553ca8a96cb
Tom (plebeius.eth) <tom@plebbit.com>
2025-05-07 12:18:54 +0200

    make useReplies page access dynamic to improve API compatibility

--- src/hooks/use-replies.ts
+++ src/hooks/use-replies.ts
@@ -8,20 +8,25 @@ const useRepliesAndAccountReplies = (comment: Comment) => {
 
   // the account's replies have a delay before getting published, so get them locally from accountComments instead
   const accountRepliesNotYetPublished = useMemo(() => {
-    const replies = comment?.replies?.pages?.best?.comments || [];
-    const replyCids = new Set(replies.map((reply: Comment) => reply?.cid));
+    const pageValues = comment?.replies?.pages ? Object.values(comment.replies.pages) : [];
+    const firstPageObject = pageValues[0] as { comments?: Comment[] } | undefined;
+    const publishedComments = firstPageObject?.comments || [];
+    const replyCids = new Set(publishedComments.map((reply: Comment) => reply?.cid));
     // filter out the account comments already in comment.replies, so they don't appear twice
     return accountComments.filter((accountReply) => !replyCids.has(accountReply?.cid));
-  }, [comment?.replies?.pages?.best?.comments, accountComments]);
+  }, [comment?.replies?.pages, accountComments]);
 
   const repliesAndNotYetPublishedReplies = useMemo(() => {
+    const pageValues = comment?.replies?.pages ? Object.values(comment.replies.pages) : [];
+    const firstPageObject = pageValues[0] as { comments?: Comment[] } | undefined;
+    const publishedComments = firstPageObject?.comments || [];
     return [
       // put the author's unpublished replies at the top, latest first (reverse)
       ...accountRepliesNotYetPublished.reverse(),
       // put the published replies after,
-      ...(comment?.replies?.pages?.best?.comments || []),
+      ...publishedComments,
     ];
-  }, [comment?.replies?.pages?.best?.comments, accountRepliesNotYetPublished]);
+  }, [comment?.replies?.pages, accountRepliesNotYetPublished]);
 
   return repliesAndNotYetPublishedReplies;
 };
