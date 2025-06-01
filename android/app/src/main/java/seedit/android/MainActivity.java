4e69723129dcf1a505b5b0cf65624e9c6629186b
Tom (plebeius.eth) <tom@plebbit.com>
2025-06-01 19:59:48 +0200

    fix(android): resolve fullscreen overlay preventing user interaction
    Resolved thanks to Capacitor community suggestion: https://forum.ionicframework.com/t/status-bar-overlaps-the-app-content-help/247967

--- android/app/src/main/java/seedit/android/MainActivity.java
+++ android/app/src/main/java/seedit/android/MainActivity.java
@@ -3,6 +3,7 @@ package seedit.android;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
+import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -11,5 +12,6 @@ public class MainActivity extends BridgeActivity {
         // Register custom and community plugins after initialization
         registerPlugin(FileUploaderPlugin.class);
         registerPlugin(LocalNotificationsPlugin.class);
+        registerPlugin(StatusBarPlugin.class);
     }
 }
